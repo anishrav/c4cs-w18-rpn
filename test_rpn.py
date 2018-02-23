5dc507b2670a9409e713058453e5b297d7ad1091
anishrav <anishrav@umich.edu>
2018-02-23 10:55:57 -0500

    Added tests formultiply, divide, exponent, divide by 0

--- test_rpn.py
+++ test_rpn.py
@@ -2,15 +2,27 @@ import unittest
 import rpn
 
 class TestBasics(unittest.TestCase):
-    def test_add(self):
-        result = rpn.calculate('1 1 +')
-        self.assertEqual(2, result)
-    def testadds(self):
-    		result = rpn.calculate('1 1 + 2 + ')
-    		self.assertEqual(4, result)
-    def test_subtract(self):
-    		result = rpn.calculate('5 2 -')
-    		self.assertEqual(3, result)
-    def test_toomany(self):
-	    	with self.assertRaises(TypeError):
-	    		result = rpn.calculate('1 2 3 +')
+	def test_add(self):
+		result = rpn.calculate('1 1 +')
+		self.assertEqual(2, result)
+	def test_adds(self):
+		result = rpn.calculate('1 1 + 2 + ')
+		self.assertEqual(4, result)
+	def test_subtract(self):
+		result = rpn.calculate('5 2 -')
+		self.assertEqual(3, result)
+	def test_toomany(self):
+		with self.assertRaises(TypeError):
+			result = rpn.calculate('1 2 3 +')
+	def test_multiply(self):
+		result = rpn.calculate('4 3 *')
+		self.assertEqual(12, result)
+	def test_divide(self):
+		result = rpn.calculate('12 3 /')
+		self.assertEqual(4, result)
+	def test_exponent(self):
+		result = rpn.calculate('4 2 ^')
+		self.assertEqual(16, result)
+	def test_div_by_0(self):
+		with self.assertRaises(TypeError):
+			result = rpn.calculate('4 0 /')
